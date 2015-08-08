e1a4aa0dda2c0c5107d817f30ec3e1bd93844053
Daniel Pereira <danielpereirabp@gmail.com>
2015-08-07 23:06:47 -0300

    Correção na migration que cria a tabela project_members

--- database/migrations/2015_08_07_172652_create_project_members_table.php
+++ database/migrations/2015_08_07_172652_create_project_members_table.php
@@ -13,9 +13,9 @@ class CreateProjectMembersTable extends Migration
     public function up()
     {
         Schema::create('project_members', function (Blueprint $table) {
-            $table->integer('project_id');
+            $table->integer('project_id')->unsigned();
             $table->foreign('project_id')->references('id')->on('projects');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
