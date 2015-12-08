fad884a396a87340b88da97f1a589076de5225e0
Daniel Pereira <danielpereirabp@gmail.com>
2015-12-08 14:03:33 -0200

    Refatoração do código

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -6,5 +6,8 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 
 interface ProjectRepository extends RepositoryInterface
 {
-
+	public function isOwner($projectId, $userId);
+	public function isMember($projectId, $userId);
+	public function addMember($projectId, $memberId);
+	public function removeMember($projectId, $memberId);
 }
